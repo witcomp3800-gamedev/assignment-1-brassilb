d4e29f02480e4dcdff1b90e672bfecc03729f780
BBBrassil <113615066+brassilbatwit@users.noreply.github.com>
2024-05-15 13:38:31 -0400

    added reading entities from file (read only)

--- src/main.cpp
+++ src/main.cpp
@@ -53,10 +53,130 @@ namespace a1 {
 	};
 
 	/**
+	 * 2-dimensional position vector.
+	 */
+	struct Position {
+		float x = 0.0f;
+		float y = 0.0f;
+
+		/**
+		 * Creates a default position with coordinates (0, 0).
+		 */
+		Position() = default;
+
+		/**
+		 * Creates a position with the specified coordinates.
+		 * @param x X-coordinate
+		 * @param y Y-coordinate
+		 */
+		Position(float x, float y) : x(x), y(y) {}
+	};
+
+	/**
+	 * 2-dimensional velocity vector.
+	 */
+	struct Velocity {
+		float x = 0.0f;
+		float y = 0.0f;
+
+		/**
+		 * Creates default zero velocity.
+		 */
+		Velocity()  = default;
+
+		/**
+		 * Creates a velocity with the specified value.
+		 * @param x X-component
+		 * @param y Y-component
+		 */
+		Velocity(float x, float y) : x(x), y(y) {}
+	};
+
+	/**
+	 * Represents a 2-dimensional shape which can be drawn to the screen with raylib.
+	 */
+	class Shape {
+	public:
+		/*
+		 * Calculates how much a shape overflows a window given a position and scale.
+		 * @param window Containing window
+		 * @param position Coordinates in pixels
+		 * @param scale Scale factor
+		 * @return Overflow in pixels
+		 */
+		virtual Vector2 calc_overflow(Window window, Position position, float scale) const = 0;
+
+		/**
+		 * Draws a shape with specified position, scale, and color.
+		 * @param position Coordinates in pixels
+		 * @param scale Scale factor
+		 * @param color Fill color
+		 */
+		virtual void draw(Position position, float scale, Color color) const = 0;
+	};
+
+	/**
+	 * Represents a circle which can be drawn to the screen with raylib.
+	 */
+	class Circle : public Shape {
+	public:
+		float radius = 1.0f;
+
+		/**
+		 * Creates a default circle with radius 1.
+		 */
+		Circle() = default;
+
+		/**
+		 * Creates a circle with the specified radius.
+		 * @param Radius
+		 * @warning The effect of creating a circle with negative radius is undefined.
+		 */
+		Circle(float radius) : radius(radius) {}
+
+		// TODO
+		Vector2 calc_overflow(Window window, Position position, float scale) const override { return {}; }
+		void draw(Position position, float scale, Color color) const override {}
+	};
+
+	/**
+	 * Represents a rectangle which can be drawn to the screen with raylib.
+	 */
+	class Rectangle : public Shape {
+	public:
+		float width = 1.0f;
+		float height = 1.0f;
+
+		/**
+		 * Creates a default rectangle with width and height 1.
+		 */
+		Rectangle() = default;
+
+		/**
+		 * Creates a rectangle with the specified width and height.
+		 * @param width Width
+		 * @param height Height
+		 * @warning The effect of creating a rectangle with negative width or height is undefined.
+		 */
+		Rectangle(float width, float height) : width(width), height(height) {}
+
+		// TODO
+		Vector2 calc_overflow(Window window, Position position, float scale) const override { return {}; }
+		void draw(Position position, float scale, Color color) const override {}
+	};
+
+	/**
 	 *
 	 */
 	class Entity {
-		// TODO
+	public:
+		std::string name;
+		Position position;
+		Velocity velocity;
+		std::unique_ptr<Shape> shape;
+		float scale;
+		Color color;
+		bool is_active;
 	};
 
 	/**
@@ -79,6 +199,28 @@ namespace a1 {
 	 * @param obj Game config
 	 */
 	std::istream& operator >>(std::istream& input, Config& obj);
+
+	/**
+	 * Reads an entity with a circle from an input stream.
+	 * @param input Input file stream
+	 * @param obj Game entity
+	 */
+	std::istream& read_circle_entity(std::istream& input, Entity& obj);
+
+
+	/**
+	 * Reads an entity's common components (i.e. not the shape) from an input stream.
+	 * @param input Input file stream
+	 * @param obj Game entity
+	 */
+	std::istream& read_common_components(std::istream& input, Entity& obj);
+
+	/**
+	 * Reads an entity with a rectangle from an input stream.
+	 * @param input Input file stream
+	 * @param obj Game entity
+	 */
+	std::istream& read_rectangle_entity(std::istream& input, Entity& obj);
 }
 
 //------------------------------------------------------------------------------------
@@ -91,11 +233,20 @@ int main(void) {
 	auto input_file = std::ifstream{ input_path };
 	a1::Config config;
 	input_file >> config;
-	if( !input_file ) {
+	if( !input_file && !input_file.eof() ) {
 		std::cerr << "Failed to load config from path " << input_path << ". Exiting...\n";
 		return -1;
 	}
 	auto& [window, font_asset, entity_templates] = config;
+
+	// Test if entity shapes were read correctly
+	std::cout << "Read " << entity_templates.size() << (entity_templates.size() == 1 ? " entity" : " entities.") << "\n";
+	std::size_t i = 0;
+	for( const auto& e : entity_templates ) {
+		const bool is_circle = dynamic_cast<a1::Circle*>(e.shape.get());
+		const bool is_rectangle = dynamic_cast<a1::Rectangle*>(e.shape.get());
+		std::cout << i++ << ": " << (is_circle ? "circle" : is_rectangle ? "rectangle" : "error") << "\n";
+	}
 
 	SetConfigFlags(FLAG_WINDOW_HIGHDPI);
 	InitWindow(window.width, window.height, window.caption.c_str());
@@ -232,12 +383,14 @@ namespace a1 {
 		std::string s;
 		while( input >> s ) {
 			if( s == "Window" ) {
+				std::cout << "Reading window...\n";
 				input
 					>> obj.window.caption
 					>> obj.window.width
 					>> obj.window.height;
 			}
 			else if( s == "Font" ) {
+				std::cout << "Reading font...\n";
 				input
 					>> obj.font_asset.file
 					>> obj.font_asset.size
@@ -245,12 +398,48 @@ namespace a1 {
 				obj.font_asset.color.a = 255;
 			}
 			else if( s == "Circle" ) {
-				// TODO
+				std::cout << "Reading circle...\n";
+				Entity circle_entity;
+				if( read_circle_entity(input, circle_entity) ) {
+					obj.entity_templates.push_back(std::move(circle_entity));
+				}
 			}
 			else if( s == "Rectangle" ) {
-				// TODO
+				std::cout << "Reading rectangle...\n";
+				Entity rectangle_entity;
+				if( read_rectangle_entity(input, rectangle_entity) ) {
+					obj.entity_templates.push_back(std::move(rectangle_entity));
+				}
 			}
-			return input;
 		}
+		return input;
+	}
+
+	std::istream& read_circle_entity(std::istream& input, Entity& entity) {
+		float radius;
+		if( read_common_components(input, entity) && input >> radius ) {
+			entity.shape = std::make_unique<Circle>(radius);
+		}
+		return input;
+	}
+
+	std::istream& read_common_components(std::istream& input, Entity& entity) {
+		input
+			>> entity.name
+			>> entity.position.x >> entity.position.y
+			>> entity.velocity.x >> entity.velocity.y
+			>> entity.color.r >> entity.color.g >> entity.color.b;
+		entity.color.a = 255;
+		entity.is_active = true;
+		return input;
+	}
+
+	std::istream& read_rectangle_entity(std::istream& input, Entity& entity) {
+		float width;
+		float height;
+		if( read_common_components(input, entity) && input >> width >> height ) {
+			entity.shape = std::make_unique<Rectangle>(width, height);
+		}
+		return input;
 	}
 }
